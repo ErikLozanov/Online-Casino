6c1ebc2939420a83e21a28ace7db9337b7f5461f
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2024-01-28 17:56:16 +0200

    added more options

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userController = require('./controllers/userController');
+const rouletteController = require('./controllers/rouletteController');
 
 router.use('/users', userController);
+router.use('/games', rouletteController);
 
 module.exports = router;
--- server2/index.js
+++ server2/index.js
@@ -1,17 +1,19 @@
+const { default: Timer } = require('easytimer.js');
 const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({ port: 8080 });
+const timer = new Timer();
 
 wss.on('connection', (ws) => {
     console.log('Incoming connection...');
 
-    const initialMessage = Math.floor(Math.random() * 14);
+    const initialMessage = Math.floor(Math.random() * 22);
     ws.send(initialMessage.toString());
 });
 
 const sendMessageToClients = () => {
     console.log('Sending message to clients...');
-    const computedRandomNum = Math.floor(Math.random() * 14);
+    const computedRandomNum = Math.floor(Math.random() * 22);
 
     wss.clients.forEach((ws) => {
         if (ws.readyState === ws.OPEN) {
@@ -20,12 +22,24 @@ const sendMessageToClients = () => {
     });
 };
 
-// Set up the interval and store the interval ID
-const intervalId = setInterval(sendMessageToClients(), 25000);
+// Set up the countdown timer to invoke sendMessageToClients every 20 seconds
+// timer.addEventListener('secondsUpdated', function (e) {
+
+// });
+
+timer.addEventListener('targetAchieved', function (e) {
+    // Reset the timer after 20 seconds
+    sendMessageToClients();
+    console.log('hi!');
+    timer.reset();
+    timer.start({ countdown: true, startValues: { seconds: 20 } });
+});
+
+// Start the timer initially
+timer.start({ countdown: true, startValues: { seconds: 20 } });
 
 // Optionally, you can add a cleanup function when the server is shutting down
 process.on('SIGINT', () => {
-    clearInterval(intervalId);
     wss.close(() => {
         console.log('WebSocket server closed.');
         process.exit();
